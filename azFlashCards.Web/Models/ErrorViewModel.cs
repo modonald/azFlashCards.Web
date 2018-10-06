f27b670b7f42ccf52fd988c319f0c25609a7ba3c
Maureen <modonald@yahoo.com>
2018-10-06 12:31:55 +0100

    Add project files.

---
+++ azFlashCards.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace azFlashCards.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
